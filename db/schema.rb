333b122295cbf1d066db3ac3561b7ea270ee5319
darrylw330 <126805312+darrylw330@users.noreply.github.com>
2023-07-04 09:27:31 +0000

    added bootstrap + shared folder/files

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_04_091421) do
+ActiveRecord::Schema.define(version: 2023_07_04_091856) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "citext"
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
@@ -57,21 +56,20 @@ ActiveRecord::Schema.define(version: 2023_07_04_091421) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.citext "email", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.citext "username"
-    t.boolean "private"
-    t.integer "likes_count", default: 0
-    t.integer "comments_count", default: 0
+    t.string "username"
+    t.boolean "private", default: true
+    t.integer "likes_count"
+    t.integer "comments_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "photos_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "photos"
